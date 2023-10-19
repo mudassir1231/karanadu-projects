a41fd294f7d47957fac05d3b7eaa8c5b8d548ecf
mohammed mudassir <88880988+mudassir1231@users.noreply.github.com>
2023-10-19 20:54:09 +0530

    Delete project_mush/project_mush directory

--- project_mush/project_mush/project1/app1/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import InputData
-
-class InputDataForm(forms.ModelForm):
-    class Meta:
-        model = InputData
-        fields = '__all__'  # Use all fields from the model
